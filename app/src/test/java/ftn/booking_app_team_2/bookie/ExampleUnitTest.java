7df61970398517341305c4f8be5bdebc9939a673
darkokos <darkosvilar02@gmail.com>
2023-11-09 19:33:05 +0100

    Created project.

---
+++ app/src/test/java/ftn/booking_app_team_2/bookie/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package ftn.booking_app_team_2.bookie;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
