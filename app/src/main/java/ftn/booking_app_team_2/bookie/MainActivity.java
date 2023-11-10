ff3fff9068ca962ec23ea06042e8122303642413
darkokos <darkosvilar02@gmail.com>
2023-11-10 20:31:22 +0100

    feat: added light and dark theme

--- app/src/main/java/ftn/booking_app_team_2/bookie/MainActivity.java
+++ app/src/main/java/ftn/booking_app_team_2/bookie/MainActivity.java
@@ -3,6 +3,7 @@ package ftn.booking_app_team_2.bookie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
