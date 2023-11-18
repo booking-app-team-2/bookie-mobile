2da13976a01f4e9b429cf2cef7a9fc1c9a5b8534
darkokos <darkosvilar02@gmail.com>
2023-11-18 18:15:37 +0100

    feat/functional navbar for all user types added
    Fixed merge conflict during rebase
    
    Fixed merge conflict

--- app/src/main/java/ftn/booking_app_team_2/bookie/fragments/ReportedUsersScreenFragment.java
+++ app/src/main/java/ftn/booking_app_team_2/bookie/fragments/ReportedUsersScreenFragment.java
@@ -2,23 +2,20 @@ package ftn.booking_app_team_2.bookie.fragments;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-import ftn.booking_app_team_2.bookie.databinding.FragmentAccountScreenBinding;
+import ftn.booking_app_team_2.bookie.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AccountScreenFragment#newInstance} factory method to
+ * Use the {@link ReportedUsersScreenFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AccountScreenFragment extends Fragment {
+public class ReportedUsersScreenFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,9 +25,8 @@ public class AccountScreenFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    private FragmentAccountScreenBinding binding;
 
-    public AccountScreenFragment() {
+    public ReportedUsersScreenFragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +36,11 @@ public class AccountScreenFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment AccountScreenFragment.
+     * @return A new instance of fragment ReportedUsersScreenFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AccountScreenFragment newInstance(String param1, String param2) {
-        AccountScreenFragment fragment = new AccountScreenFragment();
+    public static ReportedUsersScreenFragment newInstance(String param1, String param2) {
+        ReportedUsersScreenFragment fragment = new ReportedUsersScreenFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -62,25 +58,9 @@ public class AccountScreenFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentAccountScreenBinding.inflate(inflater, container, false);
-
-        return binding.getRoot();
-    }
-
-    @Override
-    public void onViewCreated(@Nullable View view, @Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        Button logOutButton = binding.logOutBtn;
-        logOutButton.setOnClickListener(view1 -> {
-            // TODO: Add logic for logging the user out.
-        });
-
-        Button changeDataButton = binding.changeDataBtn;
-        changeDataButton.setOnClickListener(view1 -> {
-            // TODO: Trigger action to change fragment.
-        });
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reported_users_screen, container, false);
     }
 }
