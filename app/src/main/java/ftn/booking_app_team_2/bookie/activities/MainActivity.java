69333e5b88ae7ee44261e75d190b08e0266e01fa
darkokos <darkosvilar02@gmail.com>
2024-01-19 15:04:09 +0100

    feat: theme-changing light sensor

--- app/src/main/java/ftn/booking_app_team_2/bookie/activities/MainActivity.java
+++ app/src/main/java/ftn/booking_app_team_2/bookie/activities/MainActivity.java
@@ -1,12 +1,18 @@
 package ftn.booking_app_team_2.bookie.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.NavController;
 import androidx.navigation.NavGraph;
 import androidx.navigation.NavInflater;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -15,10 +21,59 @@ import ftn.booking_app_team_2.bookie.R;
 import ftn.booking_app_team_2.bookie.databinding.ActivityMainBinding;
 import ftn.booking_app_team_2.bookie.tools.SessionManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private ActivityMainBinding binding;
     private NavController navController;
     private BottomNavigationView bottomNavigationView;
+
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
+    private static final int SENSOR_SENSITIVITY = 100;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (lightSensor != null)
+            sensorManager.registerListener(
+                    this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL
+            );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        sensorManager.unregisterListener(this);
+    }
+
+    private boolean isInDarkMode() {
+        int mode = AppCompatDelegate.getDefaultNightMode();
+        return mode == AppCompatDelegate.MODE_NIGHT_YES ||
+                mode == AppCompatDelegate.MODE_NIGHT_UNSPECIFIED;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_LIGHT) {
+            if (
+                    sensorEvent.values[0] >= -SENSOR_SENSITIVITY &&
+                            sensorEvent.values[0] <= SENSOR_SENSITIVITY
+            ) {
+                if (isInDarkMode())
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+            else {
+                if (!isInDarkMode())
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 
     private void setupNavbarBasedOnUserType() {
         SessionManager sessionManager = new SessionManager(this);
@@ -52,6 +107,11 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        if (sensorManager != null)
+            lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
 
         bottomNavigationView = binding.bottomNavigationView;
 
